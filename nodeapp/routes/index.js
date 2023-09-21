d64900e1e9b5fe1ececc80dc52c3f4792ba0c0ce
Javier <jamg44@gmail.com>
2023-09-21 20:33:26 +0200

    variables en vistas

--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -3,6 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  res.locals.texto = 'Hola';
+  res.locals.nombre = '<script>alert("inyección de código")</script>'
+
+  const ahora = new Date();
+  res.locals.esPar = (ahora.getSeconds() % 2) === 0;
+  res.locals.segundoActual = ahora.getSeconds();
+
+  res.locals.usuarios = [
+    { nombre: 'Smith', edad: 32 },
+    { nombre: 'Jones', edad: 27 }
+  ]
+
   res.render('index');
 });
 
