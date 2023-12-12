b4852643e1e08b18a6a1c8ebe8c7a602ec0de595
Javier <jamg44@gmail.com>
2023-12-12 22:57:53 +0100

    ponemos el email del usuario en la página privada

--- nodeapp/controllers/PrivadoController.js
+++ nodeapp/controllers/PrivadoController.js
@@ -1,6 +1,26 @@
+const Usuario = require('../models/Usuario');
+const createError = require('http-errors');
+
 class PrivadoController {
-  index(req, res, next) {
-    res.render('privado');
+  async index(req, res, next) {
+
+    try {
+      // obtener el id del usuario de la sesión
+      const usuarioId = req.session.usuarioLogado;
+
+      // buscar el usuario en la BD
+      const usuario = await Usuario.findById(usuarioId);
+
+      if (!usuario) {
+        next(createError(500, 'usuario no encontrado'))
+        return;
+      }
+
+      res.render('privado', { email: usuario.email });
+
+    } catch (err) {
+      next(err);
+    }
   }
 }
 
