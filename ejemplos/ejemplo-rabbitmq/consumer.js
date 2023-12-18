a1114f1a1ecdf8acf81f3eb34528afaa1775c4e8
Javier <jamg44@gmail.com>
2023-12-18 22:46:07 +0100

    ejemplo rabbitmq

---
+++ ejemplos/ejemplo-rabbitmq/consumer.js
@@ -0,0 +1,34 @@
+'use strict';
+
+require('dotenv').config();
+
+const amqplib = require('amqplib');
+
+const QUEUE = 'tasks';
+
+main().catch(err => console.log('Hubo un error', err));
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function main() {
+  // conectar al broker de RabbitMQ
+  const connection = await amqplib.connect(process.env.RABBITMQ_BROKER_URL);
+
+  // crear un canal
+  const canal = await connection.createChannel();
+
+  // asegurar que existe la cola para recibir mensajes
+  await canal.assertQueue(QUEUE, {
+    durable: true, // the queue will survive broker restarts
+  });
+
+  canal.prefetch(1); // pending ack's
+
+  canal.consume(QUEUE, async mensaje => {
+    const payload = mensaje.content.toString();
+    console.log(payload);
+    await sleep(150);
+    canal.ack(mensaje);
+  });
+
+}
