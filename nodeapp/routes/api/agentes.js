4500799cf600b8f7672cb0dcb2c97b55bfb9579d
Javier <jamg44@gmail.com>
2023-12-21 21:49:05 +0100

    ejemplo de upload de avatar de los agentes en el API

--- nodeapp/routes/api/agentes.js
+++ nodeapp/routes/api/agentes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Agente = require('../../models/Agente');
+const upload = require('../../lib/uploadConfigure');
 
 /**
  * @openapi
@@ -90,12 +91,17 @@ router.put('/:id', async (req, res, next) => {
 
 // POST /api/agentes
 // Crea un agente
-router.post('/', async (req, res, next) => {
+router.post('/', upload.single('avatar') , async (req, res, next) => {
   try {
     const agenteData = req.body;
+    const usuarioIdLogado = req.usuarioLogadoAPI;
+
+    console.log(req.file);
 
     // creamos una instancia de agente en memoria
     const agente = new Agente(agenteData);
+    agente.avatar = req.file.filename;
+    agente.owner = usuarioIdLogado;
 
     // la persistimos en la BD
     const agenteGuardado = await agente.save();
